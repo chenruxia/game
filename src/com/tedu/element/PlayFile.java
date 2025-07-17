62810bd548b69bc59b0954d095bba6d1c24079d0
chenruxia <148569215+chenruxia@users.noreply.github.com>
2025-07-17 17:42:03 +0800

    Update PlayFile.java

--- src/com/tedu/element/PlayFile.java
+++ src/com/tedu/element/PlayFile.java
@@ -1,110 +1,3 @@
-//package com.tedu.element;
-//
-//import java.awt.Color;
-//import java.awt.Graphics;
-//
-//import javax.swing.ImageIcon;
-//
-//import com.tedu.manager.ElementManager;
-//import com.tedu.manager.GameElement;
-//
-///*
-// * 玩家子弹类 本类的实体对象是由玩家对象调用和创建
-// * 
-// * */
-//
-//public class PlayFile extends ElementObj{
-//	private int attack = 1;//攻击力
-////	private int moveNum=30;//移动速度值
-//	private String fx;
-//	//可以扩展出多种子弹，玩家类就需要要有子弹类型
-////	private PlayFile(int x,int y,int w,int h,ImageIcon icon, String fx) {
-////		super(x,y,w,h,icon);
-////		this.attack=1;
-////		this.moveNum=3;
-////		this.fx=fx;
-////	}
-//	public PlayFile() {
-//		this.setSpeed(10);
-//	}//一个空的构造方法
-//	
-//	@Override
-//	public ElementObj createElement(String str) {
-//		String[] split=str.split(",");
-//		for(String str1:split) {
-//			String[] split2 = str1.split(":");
-//			switch(split2[0]) {
-//			case "x":this.setX(Integer.parseInt(split2[1]));break;
-//			case "y":this.setY(Integer.parseInt(split2[1]));break;
-//			case "f":this.fx = split2[1];break;
-//			}
-//		}
-//		this.setW(10);
-//		this.setH(20);
-//		return this;
-//	}
-//	@Override
-//	public void showElement(Graphics g) {
-//		g.setColor(Color.yellow);
-////		int x=this.getX();
-////		switch(this.fx) {
-////		case"up": x+=20;break;
-////		}
-//		g.fillOval(this.getX(), this.getY(),this.getW(), this.getH());
-//		
-//	}
-//	
-//	@Override
-//	protected void move() {
-//		if(this.getY()<0) {
-//			this.setLive(false);
-//			return;
-//		}
-//		this.setY(this.getY()-this.getSpeed());
-//	}
-//	public int getAttack() {
-//		return attack;
-//	}
-//	
-////	@Override
-////	protected void move() {
-////		if(this.getX()<0 || this.getX()>755 ||
-////				this.getY()<0 || this.getY()>600) {
-////			this.setLive(false);
-////			return;
-////		}
-////		
-////		switch(this.fx) {
-////		case "up":this.setY(this.getY()-this.moveNum);break;
-////		case "left":this.setX(this.getX()-this.moveNum);break;
-////		case "right":this.setX(this.getX()+this.moveNum);break;
-////		case "down":this.setY(this.getY()+this.moveNum);break;
-////		}
-////	}
-//	/*
-//	 * 对于子弹来说：1.出边界 2.碰撞 3.玩家放保险
-//	 * 处理方式就是，当达到死亡条件时，只进行修改死亡状态的操作。
-//	 * */
-////	@Override
-////	public void die() {
-////		ElementManager em=ElementManager.getManager();
-////		ImageIcon icon=new ImageIcon("image/tank/play2/player2_up.png");
-////		ElementObj obj=new Play(this.getX(),this.getY(),50,50,icon);
-////		em.addElement(obj,GameElement.PLAY);
-////	}
-//	
-////	/*
-////	 * 子弹变装*/
-////	private long time=0;
-////	protected void updateImage(long gameTime) {
-////		if(gameTime-time>20) {
-////			time=gameTime;//为下一次便装做准备
-////			this.setW(this.getW()+2);
-////			this.setH(this.getH()+2);
-////		}
-////	}
-//}
-
 
 
 package com.tedu.element;
@@ -112,9 +5,7 @@ package com.tedu.element;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
-
 import javax.swing.ImageIcon;
-
 import com.tedu.manager.ElementManager;
 import com.tedu.manager.GameElement;
 import com.tedu.manager.GameLoad;
@@ -125,6 +16,10 @@ public class PlayFile extends ElementObj {
     private int bulletType;  // 子弹类型（1-32）
     private Color bulletColor; // 子弹颜色（备用）
     private Random random = new Random();
+    private String mode = "straight"; // straight, fan, circle, curve, bomb
+    private double angle = 90; // 90度向上
+    private int tick = 0;
+    private int bombRadius = 60; // 爆炸半径
 
     public PlayFile() {
         this.setSpeed(10); // 默认速度
@@ -133,141 +28,58 @@ public class PlayFile extends ElementObj {
 
     @Override
     public ElementObj createElement(String str) {
-    	//默认方向为“up”
-    	this.fx = "up";
-    	
-        // 解析参数（格式示例："x:100,y:200,f:up,type:3"）
+        this.fx = "up";
+        this.mode = "straight";
+        this.angle = 90;
+        this.bombRadius = 60;
+        // 解析参数
         String[] split = str.split(",");
         for(String param : split) {
-            String[] keyValue = param.split(":");
-            switch(keyValue[0]) {
-                case "x": this.setX(Integer.parseInt(keyValue[1])); break;
-                case "y": this.setY(Integer.parseInt(keyValue[1])); break;
-                case "f": this.fx = keyValue[1]; break;
-                case "type": 
-                    this.bulletType = Integer.parseInt(keyValue[1]);
-                    break;
+            String[] kv = param.split(":");
+            if(kv[0].equals("angle")) angle = Double.parseDouble(kv[1]);
+            if(kv[0].equals("mode")) mode = kv[1];
+            if(kv[0].equals("radius")) bombRadius = Integer.parseInt(kv[1]);
+            switch(kv[0]) {
+                case "x": this.setX(Integer.parseInt(kv[1])); break;
+                case "y": this.setY(Integer.parseInt(kv[1])); break;
+                case "f": this.fx = kv[1]; break;
+                case "type": this.bulletType = Integer.parseInt(kv[1]); break;
             }
         }
-
-        // 如果未指定类型，随机选择1-16
         if(bulletType < 1 || bulletType > 32) {
             bulletType = random.nextInt(16) + 1;
         }
-
-        // 配置子弹属性
         configureBullet();
-        
         this.setW(10);
         this.setH(20);
         return this;
     }
 
     private void configureBullet() {
-    	
-//    	根据子弹类型选择图片（1-32号）
-    	String imgKey = "bullet" + bulletType;//对应fire文件夹中的图片
-    	ImageIcon icon = GameLoad.imgMap.get(imgKey);
-    	
-    	if(icon != null) {
-    		this.setIcon(icon);
-    		this.setW(icon.getIconWidth());
-    		this.setH(icon.getIconHeight());
-    	}else {
-//    		默认图形
-    		setDefaultAppearance();
-    	}
-    	
-    	switch(bulletType % 8) {
-    	case 1:
-    		this.attack = 1;
-    		this.setSpeed(10);
-    		break;
-    	case 2:
-    		this.attack = 1;
-    		this.setSpeed(15);
-    		break;
-    	case 3:
-    		this.attack = 1;
-    		this.setSpeed(8);
-    		break;
-    	case 4:
-    		this.attack = 2;
-    		this.setSpeed(7);
-    		break;
-    	case 5:
-    		this.attack = 1;
-    		this.setSpeed(12);
-    		break;
-    	case 6:
-    		this.attack = 3;
-    		this.setSpeed(5);
-    		break;
-    	case 7:
-    		this.attack = 1;
-    		this.setSpeed(9);
-    		break;
-    	case 0:
-    		this.attack = 2;
-    		this.setSpeed(6);
-    		break;
-    	}
-    	
-    	
-    	
-//        // 尝试从资源加载图片
-//        ImageIcon icon = GameLoad.imgMap.get("fire"+bulletType);
-//        
-//        if(icon != null) {
-//            // 使用图片资源
-//            this.setIcon(icon);
-//            this.setW(icon.getIconWidth());
-//            this.setH(icon.getIconHeight());
-//        } else {
-//            // 无图片资源时使用彩色图形
-//            setDefaultAppearance();
-//        }
-//
-//        // 根据类型设置特殊属性
-//        switch(bulletType) {
-//            case 1: case 2: case 3:   // 普通子弹
-//                this.attack = 1;
-//                this.setSpeed(10);
-//                break;
-//                
-//            case 4: case 5: case 6:   // 快速子弹
-//                this.attack = 1;
-//                this.setSpeed(15);
-//                break;
-//                
-//            case 7: case 8: case 9:   // 强力子弹
-//                this.attack = 2;
-//                this.setSpeed(8);
-//                break;
-//                
-//            case 10: case 11: case 12: // 散射子弹
-//                this.attack = 1;
-//                this.setSpeed(12);
-//                break;
-//                
-//            case 13: case 14: case 15: // 穿透子弹
-//                this.attack = 1;
-//                this.setSpeed(9);
-//                break;
-//                
-//            case 16:                  // 特殊子弹
-//                this.attack = 3;
-//                this.setSpeed(7);
-//                break;
-//                
-//            default:                  // 17-32号子弹
-//                this.attack = 1 + (bulletType % 3);
-//                this.setSpeed(8 + (bulletType % 5));
-//        }
+        String imgKey = "bullet" + bulletType;
+        ImageIcon icon = GameLoad.imgMap.get(imgKey);
+        if(icon != null) {
+            this.setIcon(icon);
+            this.setW(icon.getIconWidth());
+            this.setH(icon.getIconHeight());
+        } else {
+            setDefaultAppearance();
+        }
+        switch(bulletType % 8) {
+            case 1: this.attack = 1; this.setSpeed(10); break;
+            case 2: this.attack = 1; this.setSpeed(8); break;
+            case 3: this.attack = 1; this.setSpeed(6); break;
+            case 4: this.attack = 2; this.setSpeed(7); break;
+            case 5: this.attack = 1; this.setSpeed(12); break;
+            case 6: this.attack = 3; this.setSpeed(5); break;
+            case 7: this.attack = 1; this.setSpeed(9); break;
+            case 0: this.attack = 2; this.setSpeed(6); break;
+        }
+        // 爆炸弹可适当降低速度
+        if(mode.equals("bomb")) this.setSpeed(4);
     }
 
     private void setDefaultAppearance() {
-        // 无图片时使用彩色图形
         switch(bulletType % 7) {
             case 0: bulletColor = Color.YELLOW; break;
             case 1: bulletColor = Color.RED; break;
@@ -277,29 +89,19 @@ public class PlayFile extends ElementObj {
             case 5: bulletColor = Color.MAGENTA; break;
             case 6: bulletColor = Color.ORANGE; break;
         }
-        
-        // 设置默认大小
         if(bulletType <= 16) {
-            this.setW(10); 
-            this.setH(20);
+            this.setW(10); this.setH(20);
         } else {
-            this.setW(15);
-            this.setH(25);
+            this.setW(15); this.setH(25);
         }
     }
 
     @Override
     public void showElement(Graphics g) {
         if(this.getIcon() != null) {
-            // 有图片资源时绘制图片
-            g.drawImage(this.getIcon().getImage(), 
-                       this.getX(), this.getY(), 
-                       this.getW(), this.getH(), null);
+            g.drawImage(this.getIcon().getImage(), this.getX(), this.getY(), this.getW(), this.getH(), null);
         } else {
-            // 无图片时绘制彩色图形
             g.setColor(bulletColor);
-            
-            // 根据类型绘制不同形状
             if(bulletType % 3 == 0) {
                 g.fillRect(this.getX(), this.getY(), this.getW(), this.getH());
             } else if(bulletType % 3 == 1) {
@@ -314,39 +116,49 @@ public class PlayFile extends ElementObj {
 
     @Override
     protected void move() {
-    	System.out.println("子弹方向: " + fx + ", 坐标: " + getX() + "," + getY());
-    	if(fx==null) {
-    		this.setLive(false);
-    		return;
-    	}
-        // 根据方向移动
-        switch(fx) {
-            case "up":
-                this.setY(this.getY() - this.getSpeed());
-                if(this.getY() < 0) this.setLive(false);
+        tick++;
+        int speed = getSpeed();
+        double rad = Math.toRadians(angle);
+        int dx = 0, dy = 0;
+        switch(mode) {
+            case "straight":
+                dx = (int)(Math.cos(rad) * speed);
+                dy = (int)(-Math.sin(rad) * speed);
                 break;
-                
-            case "down":
-                this.setY(this.getY() + this.getSpeed());
-                if(this.getY() > 600) this.setLive(false);
+            case "fan":
+                dx = (int)(Math.cos(rad) * speed);
+                dy = (int)(-Math.sin(rad) * speed);
                 break;
-                
-            case "left":
-                this.setX(this.getX() - this.getSpeed());
-                if(this.getX() < 0) this.setLive(false);
+            case "circle":
+                dx = (int)(Math.cos(rad) * speed);
+                dy = (int)(-Math.sin(rad) * speed);
                 break;
-                
-            case "right":
-                this.setX(this.getX() + this.getSpeed());
-                if(this.getX() > 800) this.setLive(false);
+            case "curve":
+                dx = (int)(Math.sin(tick / 10.0) * 5);
+                dy = -speed;
+                break;
+            case "bomb":
+                dx = 0;
+                dy = -speed;
                 break;
         }
+        setX(getX() + dx);
+        setY(getY() + dy);
+        if(getY() < 0 || getX() < 0 || getX() > 800) setLive(false);
     }
 
     public int getAttack() {
         return attack;
     }
-    
+
+    public String getMode() {
+        return mode;
+    }
+
+    public int getBombRadius() {
+        return bombRadius;
+    }
+
     // 子弹碰撞后是否消失（穿透弹不消失）
     public boolean isDisappearAfterHit() {
         return bulletType != 13 && bulletType != 14 && bulletType != 15;
